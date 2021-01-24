cc8a2c35c910dc633a2b68bbfb25ceab2df2df36
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-01-24 13:19:31 -0700

    added state and county data split by month

--- convert-data.js
+++ convert-data.js
@@ -1,9 +1,72 @@
 const csv=require('csvtojson')
 const stateData = "nyt-data/us-states.csv";
+const countyData = "nyt-data/us-counties.csv";
 const usData = "nyt-data/us.csv";
 const fs = require('fs');
 
-convertStateData()
+convertCountyData();
+convertStateData();
+
+async function convertCountyData(){
+  let rawData = await csv().fromFile(countyData);
+  const fips = JSON.parse(fs.readFileSync('data/fips-county.json', {encoding:'utf8', flag:'r'}));
+  let jsonData = {};
+  // console.log(rawData.length);
+  for(const row of rawData){
+    if(fips[row.fips] == undefined){
+      continue;
+    }
+    let s = fips[row.fips].state;
+    if(jsonData[s]==null){
+      jsonData[s] = {}
+    }
+    if(jsonData[s][row.date]==null){
+      jsonData[s][row.date] = {}
+    }
+    jsonData[s][row.date][row.fips] = {
+      totalCases: Number(row.cases),
+      totalDeaths: Number(row.deaths),
+    }
+    let yesterdayState = jsonData[s];
+    if(yesterdayState==null){
+      yesterdayState = {}
+    }
+    let yesterday = yesterdayState[getPreviousDay(new Date(row.date))];
+    if(yesterday==null){
+      yesterday = {}
+    }
+    let yesterdayCounty = yesterday[row.fips];
+    if(yesterdayCounty == null){
+      yesterdayCounty = {}
+    }
+    jsonData[s][row.date][row.fips].cases = row.cases - (yesterdayCounty.totalCases || 0);
+    jsonData[s][row.date][row.fips].deaths = row.deaths - (yesterdayCounty.totalDeaths || 0);
+  }
+  for (const [state, stateData] of Object.entries(jsonData)) {
+    let year = 2021;
+    let month = 1;
+    let filtered = Object.keys(stateData).filter(key => key.indexOf(monthString(year, month))>-1).reduce((obj, key) => {obj[key] = stateData[key]; return obj;}, {});
+    while(year===2020 || (year===2021 && month<=1)){
+      if(Object.keys(filtered).length>0){
+        if(year === 2020){
+          filtered.final = true;
+        }
+        let dir = "data/county/"+state;
+        if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
+        }
+        fs.writeFileSync("data/county/"+state+"/"+monthString(year,month)+".json",JSON.stringify(filtered))
+      }
+      month++;
+      if(month === 13){
+        month = 1;
+        year++;
+      }
+      filtered = Object.keys(stateData).filter(key => key.indexOf(monthString(year, month))>-1).reduce((obj, key) => {obj[key] = stateData[key]; return obj;}, {});
+    }
+  }
+  console.log(jsonData["CO"]['2021-01-23']);
+}
 
 async function convertStateData(){
   let rawData = await csv().fromFile(stateData);
@@ -41,8 +104,31 @@ async function convertStateData(){
     jsonData[row.date].usCases = row.cases - (yesterday.usTotalCases || 0);
     jsonData[row.date].usDeaths = row.deaths - (yesterday.usTotalDeaths || 0);
   }
-  fs.writeFileSync("data/state.json",JSON.stringify(jsonData))
+  let year = 2021;
+  let month = 1;
+  let filtered = Object.keys(jsonData).filter(key => key.indexOf(monthString(year, month))>-1).reduce((obj, key) => {obj[key] = jsonData[key]; return obj;}, {});
+  while(Object.keys(filtered).length>0){
+    console.log(monthString(year,month), Object.keys(filtered).length);
+    if(year === 2020){
+      filtered.final = true;
+    }
+    fs.writeFileSync("data/state/"+monthString(year,month)+".json",JSON.stringify(filtered))
+    month++;
+    if(month === 13){
+      month = 1;
+      year++;
+    }
+    filtered = Object.keys(jsonData).filter(key => key.indexOf(monthString(year, month))>-1).reduce((obj, key) => {obj[key] = jsonData[key]; return obj;}, {});
+  }
+  // fs.writeFileSync("data/state.json",JSON.stringify(jsonData))
   console.log(jsonData['2021-01-22']);
+}
+
+function monthString(y, m){
+  if(m<10){
+    return y.toString()+"-0"+m.toString();
+  }
+  return y+'-'+m;
 }
 
 function getPreviousDay(d) {
